6bc585c2c8ca211acf36b33e0821684379a0a23f
Stefano <piero.quiroz93@gmail.com>
2021-07-06 16:25:19 -0500

    Select tag finished

--- assignments/luke-api-walker/src/views/Select.js
+++ assignments/luke-api-walker/src/views/Select.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Select = () => {
+
+    const [starwars, setStarWars]=useState({
+        options: []
+    })
+
+    useEffect(()=>{
+        fetch("https://swapi.dev/api/")
+            .then(res => res.json())
+            .then(res => setStarWars({
+                options: Object.entries(res)
+            }))
+    }, [])
+    console.log(starwars.options);
+    //Object.entries {propiedad} => [propiedad]
+    const option = starwars.options.map(([value, url], index) => (<option key={index} value={url}>{value.replace(/\b\w/g, c => c.toUpperCase())}</option>))
+
+
     return (
-        <div>
-            
-        </div>
+        <form>
+            <div>
+                <label htmlFor="search">Search for: </label>
+                <select id="search" name="select">
+                    {/* <option value="People">People</option>
+                    <option value="Films">Films</option>
+                    <option value="Starships">Starships</option>
+                    <option value="Vehicles">Vehicles</option>
+                    <option value="Species">Species</option> */}
+                    {option}
+                </select>
+            </div>
+            <div>
+                <label htmlFor="id">id </label>
+                <input type="number"/>
+                <input type="submit" value="Search"/>
+            </div>                        
+        </form>
     );
 }
 
